64ffa67ae229f48c443a66518af439f9ed38e6bf
Leonardo Mrtz <carlosmroder@gmail.com>
2026-04-14 12:12:10 -0600

    nd

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -4,7 +4,7 @@ import Footer from "../components/Footer";
 
 export default function MainLayout() {
   return (
-    <div className="min-h-screen bg-stone-400 px-3 py-8 text-white md:px-6 lg:px-14">
+    <div className="min-h-screen bg-stone-200 px-3 py-8 text-white md:px-6 lg:px-14">
       <div className="mx-auto max-w-7xl">
         
         <header className="mb-10 flex flex-col gap-5 md:flex-row md:items-center md:gap-105 pl-8">
